f2029de5b6e47520b29d22798f69b959495bfcdd
Rakesh-R-A <rockesh1996@gmail.com>
2017-03-20 00:53:24 +0530

    Update equalindex.java

--- equalindex.java
+++ equalindex.java
@@ -12,7 +12,7 @@ class equalindex
 		int n,i,z=0;
 		Scanner sc=new Scanner(System.in);
 		n=sc.nextInt();
-		int a[]=new int[10];
+		int a[]=new int[n];
 		Vector v=new Vector();
 		for(i=0;i<n;i++)
 		{  a[i]=sc.nextInt();
